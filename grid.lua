7e558950e8705e40d1c4ffaf95732771d41908f1
paulpls <62158353+paulpls@users.noreply.github.com>
2023-05-16 04:39:31 -0400

    simplified HSV conversion and docs

--- grid.lua
+++ grid.lua
@@ -18,22 +18,23 @@ local hsvaColor = function (h, s, v, a)
     if s <= 0 then return {v, v, v, a} end
     h = h * 6
     local c = v * s
-    local x = c * (1 - math.abs((h%2) - 1))
-    local m,r,g,b = (v-c), 0, 0, 0
-    if h < 1 then
-        r,g,b = c, x, 0
-    elseif h < 2 then
-        r,g,b = x, c, 0
-    elseif h < 3 then
-        r,g,b = 0, c, x
-    elseif h < 4 then
-        r,g,b = 0, x, c
-    elseif h < 5 then
-        r,g,b = x, 0, c
-    else
-        r,g,b = c, 0, x
-    end
-    return {r+m, g+m, b+m, a}
+    local x = c * (1 - math.abs((h % 2) - 1))
+    local m,r,g,b = v - c, 0, 0, 0
+    local colors = {
+        {c, x, 0},
+        {x, c, 0},
+        {0, c, x},
+        {0, x, c},
+        {x, 0, x},
+        {c, 0, x}
+    }
+    r,g,b = unpack(colors[math.ceil(h)])
+    return {
+        r + m,
+        g + m,
+        b + m,
+        a
+    }
 end
 
 
@@ -143,16 +144,6 @@ end
 Grid.grow = function (self, h, k, r)
     --
     --  Cache the set of points witin a circle centered at point (h,k):
-    --
-    --    .--‾‾‾‾‾--.     
-    --   |           |               __________________________
-    --  |             |             /         2             2
-    --  |     h,k     |   r == ‾\  / ( x - h )  +  ( y - k )
-    --  |      |      |          \/
-    --   |    r|     |
-    --    ‾--__|__--‾    
-    --
-    --  Solved for x:
     --                     ____________________
     --                    /   2             2
     --      x == h + ‾\  /   r  -  ( y - k )
